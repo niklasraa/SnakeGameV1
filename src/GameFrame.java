16162bb0cb08be719f82d729b5612b53e612df8d
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2021-05-04 15:08:22 +0200

    Methods added

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,6 +4,15 @@ public class GameFrame extends JFrame {
 
 	GameFrame() {
 		this.add(new GamePanel());
+		// Title for this frame
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Frame resizable by user.
+		this.setResizable(false);
+		this.pack();
+		// Show or hide the window
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,18 +1,58 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	GamePanel() {
+
+	}
+
+	public void startGame() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+
+	}
+
+	public void draw(Graphics g) {
+
+	}
+
+	public void move() {
+
+	}
+	
+	public void checkApple() {
 		
 	}
 	
-	GamePanel() {
+	public void checkCollissions() {
 		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
